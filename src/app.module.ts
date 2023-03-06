4fd19851a0e8018a247946cde879926faa1baffe
nam.nguyen@gss-sol.com <tsunamikampa@gmail.com>
2023-03-06 15:13:45 +0700

    config production

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,10 @@ import { AuthModule } from './auth/auth.module';
     }),
     MongooseModule.forRoot(
       `mongodb://${
+        process.env.DB_USERNAME +
+        ':' +
+        process.env.DB_PASSWORD +
+        '@' +
         process.env.DB_HOST +
         ':' +
         process.env.DB_PORT +
